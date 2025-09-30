5bccdd0e022f7f7012afe7d4779194e17cc21a52
a012 <Aeagan0507@gmail.com>
2025-09-30 17:56:17 -0400

    Update extraStuff.ts

--- extraStuff.ts
+++ extraStuff.ts
@@ -1,12 +1,22 @@
 // Add your code here
-//% blockNamespace=advancedPxt
+//% blockNamespace=pxt
 namespace splitScreen { }
-//% blockNamespace=advancedPxt
+//% blockNamespace=extensions
 namespace dj {}
-//% blockNamespace=advancedPxt
+//% blockNamespace=extensions
 namespace shader {}
-//% blockNamespace=advancedPxt
+//% blockNamespace=pxt
 namespace spriteutil {}
+//% blockNamespace=advancedPxt
+namespace blockObject {}
+//% blockNamespace=extensions
+namespace miniMenu {}
+//% blockNamespace=pxt
+namespace textsprite {}
+//% blockNamespace=extensions
+namespace turtleLogo {}
+//% blockNamespace=extensions
+namespace story {}
 
 //% advanced=true
 declare namespace advancedPxt {
@@ -14,4 +24,6 @@ declare namespace advancedPxt {
     function dmesg(str: string): void
 }
 
+
+namespace extensions {}
 
