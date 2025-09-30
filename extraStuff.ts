da23fea7ee6ee44d91a3625c30b0029c48d69852
a012 <Aeagan0507@gmail.com>
2025-09-30 17:46:45 -0400

    Update pxt.json, extraStuff.ts

---
+++ extraStuff.ts
@@ -0,0 +1,16 @@
+// Add your code here
+//% blockNamespace=advancedPxt
+namespace splitScreen { }
+//% blockNamespace=advancedPxt
+namespace dj {}
+//% blockNamespace=advancedPxt
+namespace shader {}
+//% blockNamespace=advancedPxt
+namespace spriteutil {}
+
+declare namespace advancedPxt {
+    //% block="dmesg" shim="control::dmesg"
+    function dmesg(): void
+}
+
+
